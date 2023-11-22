9c836d154f293e15a8da1cfc0e856ed49324889f
a1866621 <a1866621@adelaide.edu.au>
2023-11-22 15:39:56 +1030

    feat: second business rule

--- bernst/code/PassADay.cs
+++ bernst/code/PassADay.cs
@@ -6,4 +6,24 @@ public class PassADay
     {
         return internalValue - 1;
     }
+
+    public static int[] passADay(int[] internalValue)
+    {
+        var returnValues = new List<int>();
+        for (int i = 0; i < internalValue.Length; i++)
+        {
+            if (internalValue[i] == 0)
+            {
+                //internalValue[i] = 6;
+                returnValues.Add(6);
+                returnValues.Add(8);
+            }
+            else
+            {
+                returnValues.Add(internalValue[i] - 1);
+            }
+        }
+
+        return returnValues.ToArray();
+    }
 }
--- bernst/tests/PassADay.cs
+++ bernst/tests/PassADay.cs
@@ -5,12 +5,25 @@ public class PassADay : code.PassADay
 {
 
     [TestMethod]
-    public void passADay()
+    public void firstBusinessRule()
     {
         int internalValue = 3;
 
-        internalValue= passADay(internalValue);
+        internalValue = passADay(internalValue);
 
         Assert.AreEqual(2, internalValue);
+    }
+
+
+//After another day, its internal timer would reset to 6, and it would create a new lanternfish with an internal timer of 8.
+
+    [TestMethod]
+    public void secondBusinessRule()
+    {
+        int[] fishes = new int[] { 0 };
+
+        var expectedFishes = passADay(fishes);
+
+        CollectionAssert.AreEqual(new int[] { 6, 8 }, expectedFishes);
     }
 }
