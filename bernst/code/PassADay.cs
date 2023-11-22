54b8198c9df0b4d6534b20608ea4539bc847d5d2
a1866621 <a1866621@adelaide.edu.au>
2023-11-22 15:55:13 +1030

    feat: passed two days

--- bernst/code/PassADay.cs
+++ bernst/code/PassADay.cs
@@ -10,20 +10,37 @@ public class PassADay
     public static int[] passADay(int[] internalValue)
     {
         var returnValues = new List<int>();
+        int newFishCount = 0;
         for (int i = 0; i < internalValue.Length; i++)
         {
             if (internalValue[i] == 0)
             {
                 //internalValue[i] = 6;
                 returnValues.Add(6);
-                returnValues.Add(8);
+                newFishCount++;
             }
             else
             {
                 returnValues.Add(internalValue[i] - 1);
             }
         }
+        for (int j = 0; j < newFishCount; j++)
+        {
+            returnValues.Add(8);
+        }
 
         return returnValues.ToArray();
+    }
+
+
+    public static int[] passDays(int days, int[] fishes)
+    {
+
+        for (int i = 0; i < days; i++)
+        {
+            fishes = passADay(fishes);
+        }
+
+        return fishes;
     }
 }
--- bernst/tests/PassADay.cs
+++ bernst/tests/PassADay.cs
@@ -26,4 +26,25 @@ public class PassADay : code.PassADay
 
         CollectionAssert.AreEqual(new int[] { 6, 8 }, expectedFishes);
     }
+
+     // after 18 days, there are a total of 26 fish.
+    [TestMethod]
+    public void afterOneDay()
+    {
+        int[] fishes = new int[] { 3,4,3,1,2 };
+
+        var expectedFishes = passDays(1,fishes);
+
+        CollectionAssert.AreEqual(new int[] { 2,3,2,0,1 }, expectedFishes);
+    }
+
+        [TestMethod]
+    public void afterTwoDay()
+    {
+        int[] fishes = new int[] { 3,4,3,1,2 };
+
+        var expectedFishes = passDays(2,fishes);
+
+        CollectionAssert.AreEqual(new int[] { 1,2,1,6,0,8}, expectedFishes);
+    }
 }
