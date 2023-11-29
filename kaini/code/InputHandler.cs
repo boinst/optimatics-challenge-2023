3f6e7bbf45986ff067378c62d9f3db2513e083f5
a1866621 <a1866621@adelaide.edu.au>
2023-11-29 15:16:17 +1030

    feat: input string handler

---
+++ kaini/code/InputHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace code;
+
+public class InputHandler
+{
+
+    // store lines of strings in a 2d array
+    public static int[,] readInput(string input)
+    {
+        // Split the input into rows
+        string[] rows = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int numRows = rows.Length;
+        int numCols = rows[0].Length;
+        var heightMap = new int[numRows, numCols];
+
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < numCols; j++)
+            {
+                heightMap[i, j] = rows[i][j] - '0'; // Convert char to int
+            }
+        }
+
+
+        // // Display the 2D array for verification
+        // for (int i = 0; i < numRows; i++)
+        // {
+        //     for (int j = 0; j < numCols; j++)
+        //     {
+        //         Console.Write(heightMap[i, j]);
+        //     }
+        //     Console.WriteLine();
+        // }
+        return heightMap;
+    }
+}
---
+++ kaini/tests/kaini/InputHandlerTest.cs
@@ -0,0 +1,17 @@
+namespace tests;
+
+[TestClass]
+public class InputHandlerTest : code.InputHandler
+{
+    [TestMethod]
+    public void readInput()
+    {
+        string input =
+        @"123
+456
+789";
+        int[,] expected = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[,] actual = code.InputHandler.readInput(input);
+        CollectionAssert.AreEqual(expected, actual);
+    }
+}
