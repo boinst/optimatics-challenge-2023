765ded185f42a6ce2e9fcd0d40f804d193446d1f
a1866621 <a1866621@adelaide.edu.au>
2023-11-29 15:38:53 +1030

    feat: get the neighbour list

--- kaini/code/Compare.cs
+++ kaini/code/Compare.cs
@@ -13,4 +13,40 @@ public class Compare
         }
         return true;
     }
+
+    //get all the neighbors of a point
+    public static List<int> getNeighbors(int[,] heightMap, int row, int col)
+    {
+        // dynamic length array for neighbors
+        List<int> neighbors = new List<int>();
+        int numRows = heightMap.GetLength(0);
+        int numCols = heightMap.GetLength(1);
+
+        //up
+        if (row - 1 >= 0)
+        {
+            neighbors.Add(heightMap[row - 1, col]);
+        }
+
+        //down
+        if (row + 1 < numRows)
+        {
+            neighbors.Add(heightMap[row + 1, col]);
+        }
+
+        //left
+        if (col - 1 >= 0)
+        {
+            neighbors.Add(heightMap[row, col - 1]);
+        }
+
+        //right
+        if (col + 1 < numCols)
+        {
+            neighbors.Add(heightMap[row, col + 1]);
+        }
+
+
+        return neighbors;
+    }
 }
--- kaini/tests/kaini/CompareTest.cs
+++ kaini/tests/kaini/CompareTest.cs
@@ -13,4 +13,17 @@ public class CompareTest : code.Compare
         bool actual = code.Compare.isSmallest(currentPoint, neighbors);
         Assert.AreEqual(expected, actual);
     }
+
+
+    [TestMethod]
+    public void getNeighborsTest()
+    {
+        int[,] heightMap = { { 1, 2, 3 }, { 4, 5, 6 } };
+        int row = 0;
+        int col = 0;
+
+        List<int> expected = new List<int> { 4, 2 };
+        List<int> actual = code.Compare.getNeighbors(heightMap, row, col);
+        CollectionAssert.AreEqual(expected, actual);
+    }
 }
