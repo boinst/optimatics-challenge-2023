78a8bb5051ac2364e4b03afeff55d3c85f21a965
pratt33k <prateek28a@gmail.com>
2023-11-29 16:32:42 +1030

    Added getlowPoint2() and refactored implementation

--- bernst/tests/prateek/2021D6/test_d9.cs
+++ bernst/tests/prateek/2021D6/test_d9.cs
@@ -3,11 +3,25 @@ namespace tests.prateek._2021D6;
 [TestClass]
 public class PartOneTests
 {
+    // Given the height Map only has one number {0}
+    // The low point would be the same number 0
     [TestMethod]
-    public void getLowPoint()
+    public void getLowPoint1()
     {
-        int startState = 0;
+        int[] startState = new int[] {0};
         var expected = 0;
+
+        int actual = new D9().getLowPoint(startState);
+        Assert.AreEqual(expected, actual);
+    }
+
+    // Given the height Map only has 2 numbers {0,1}
+    // The low point would be 0
+    [TestMethod]
+    public void getLowPoint2()
+    {
+        int[] startState = new int[] {0,1};
+        int expected = 0;
 
         int actual = new D9().getLowPoint(startState);
         Assert.AreEqual(expected, actual);
