99c5a9a33cda9dcfee5b33ab1f1cd5a49656fe1b
Punlapa Panwech <punlapa3898@gmail.com>
2023-11-30 14:20:01 +1030

    test passes 1 star

--- alicia_java_branch/src/Main.java
+++ alicia_java_branch/src/Main.java
@@ -1,17 +1,97 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static int sumOfLowPoints;
+    static List<Integer> lowPointList = new ArrayList<Integer>();
+    static int[][] heightmapMatrix;
+    static int rowSize;
+    static int columnSize;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static int iteratingMatrix(String input) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        heightmapMatrix = splitInput(input);
+        rowSize = heightmapMatrix.length;
+        columnSize = heightmapMatrix[0].length;
+        sumOfLowPoints = 0;
+
+        for (int i = 0; i < heightmapMatrix.length; i++) {
+            for (int j = 0; j < heightmapMatrix[0].length; j++) {
+
+                boolean isLowPoint = isLowPoint(i, j);
+                if (isLowPoint) {
+                    lowPointList.add(heightmapMatrix[i][j]);
+                    sumOfLowPoints = sumOfLowPoints + heightmapMatrix[i][j] + 1;
+                }
+
+            }
         }
+
+        return sumOfLowPoints;
+    }
+
+    public static int[][] splitInput(String inputString) {
+
+        String[] rowArray = inputString.split("\n");
+        heightmapMatrix = new int[rowArray.length][];
+        try {
+            int r = 0;
+            for (String row : rowArray) {
+                String[] itemsArray = row.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s", "").split("");
+                int[] results = new int[itemsArray.length];
+
+                for (int i = 0; i < itemsArray.length; i++) {
+                    try {
+                        results[i] = Integer.parseInt(itemsArray[i]);
+                    } catch (NumberFormatException ignored) {
+
+                    }
+                    ;
+                }
+
+                heightmapMatrix[r++] = results;
+            }
+
+        } catch (NumberFormatException ignored) {
+
+        }
+
+        System.out.println(Arrays.deepToString(heightmapMatrix));
+
+        return heightmapMatrix;
+    }
+
+    static List<Integer> getAdjacent(int rowIndex, int columnIndex) {
+
+        List<Integer> adjacentsList = new ArrayList<>();
+
+        if (rowIndex - 1 >= 0) {
+            adjacentsList.add(heightmapMatrix[rowIndex - 1][columnIndex]);
+        }
+
+        if (columnIndex - 1 >= 0) {
+            adjacentsList.add(heightmapMatrix[rowIndex][columnIndex - 1]);
+        }
+
+        if (columnIndex + 1 < columnSize) {
+            adjacentsList.add(heightmapMatrix[rowIndex][columnIndex + 1]);
+        }
+
+        if (rowIndex + 1 < rowSize) {
+            adjacentsList.add(heightmapMatrix[rowIndex + 1][columnIndex]);
+        }
+
+        // Returning the arraylist of adjacent
+        return adjacentsList;
+    }
+
+    private static boolean isLowPoint(int rowIndex, int columnIndex) {
+
+        List<Integer> adjacentList = getAdjacent(rowIndex, columnIndex);
+        Integer minValue = Collections.min(adjacentList);
+
+        return minValue > heightmapMatrix[rowIndex][columnIndex];
     }
 }
